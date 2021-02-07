f724ff8f5e76f2c6b880e5a12ac10f9d21cc9d4d
lgrossi <lucas.ggrossi@gmail.com>
2021-02-07 23:56:12 +0100

    add env var

--- HelloWorld/src/Clients/CloudFunctionClient.php
+++ HelloWorld/src/Clients/CloudFunctionClient.php
@@ -10,13 +10,10 @@ class CloudFunctionClient
     private const HERO_CHECK = "hero-check";
     private const SLACK_MESSAGE = "slack-message";
 
-    /* This needs to be env var. */
-    private const CLOUD_FUNCTION_BASE_URI = "https://europe-west3-potent-duality-278504.cloudfunctions.net/slack-notification";
-
     public static function sendSlackMessage(string $message, string $channelId): void
     {
         (new Client())->postAsync(
-            self::CLOUD_FUNCTION_BASE_URI,
+            getenv('CLOUD_FUNCTION_BASE_URI'),
             [
                 "json" => [
                     "type" => self::SLACK_MESSAGE,
@@ -31,7 +28,7 @@ class CloudFunctionClient
     {
         try {
             return (new Client())->post(
-                self::CLOUD_FUNCTION_BASE_URI,
+                getenv('CLOUD_FUNCTION_BASE_URI'),
                 [
                     "json" => [
                         "type" => self::HERO_CHECK,
